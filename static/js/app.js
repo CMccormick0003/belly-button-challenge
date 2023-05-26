afdf3bd43f2e0e7628d6e1dd958830d228ccddc8
CMccormick0003 <calogera.mccormick@gmail.com>
2023-05-25 23:11:02 -0400

    added init function

--- static/js/app.js
+++ static/js/app.js
@@ -5,6 +5,7 @@ function buildCharts(sample){
         let resultArray = samples.filter((sampleDictionary) => sampleDictionary.id == sample);
         let result = resultArray[0];
 
+
         // Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual
 
         let otuIDs = result.otu_ids;
@@ -27,15 +28,13 @@ function buildCharts(sample){
         let barData = [barchart];
         
         let bartitle = {
-        title: `Top 10 OTUs per Individual`
-        margin: {t: 20, l: 150}
+        title: `Top 10 OTUs per Individual`,
+        margin: {t: 35, l: 150}
 
         };
 
         // Use Plotly to plot the data with the layout for the bar chart
-        Plotly.newPlot("bar", barData, bartitle);
-
-// ---------------------------------------------------------------------------------------
+        Plotly.newPlot("bar", barData, bartitle)
 
         // Create a bubble chart that displays each sample
         let bubbleLayout = {
@@ -43,10 +42,9 @@ function buildCharts(sample){
             margin: {t:0},
             hovermode: "closest",
             xaxis: {title: "Operational taxonomic unit (OTU) IDs"},
-            margin: {t:20}
+            margin: {t:35}
         };
 
-        // Create the trace for the bubble chart
         let bubbleData = [
             {  x: otuIDs,
                 y: sampleValues,
@@ -59,8 +57,12 @@ function buildCharts(sample){
                 }
             }
         ]
+        // Use Plotly to plot the data with the layout for the bubble chart
+        Plotly.newPlot("bubble", bubbleData, bubbleLayout)
+
     });
 }
+
 
 // Display the sample metadata
 
@@ -75,3 +77,24 @@ function buildMetadata(sample){
         for (key in result) {
              PANEL.append("h6").text(`${key.toUpperCase()}: ${result[key]}`)
         }
+    })
+}
+
+function init(){let selector = d3.select("#selDataset");
+d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) =>{
+let Subject_ID=data.names;
+for (let i=0;i<Subject_ID.length;i++){selector.append("option").text(Subject_ID[i]).property("value",Subject_ID[i])}
+onchange
+
+buildCharts(Subject_ID[0])
+buildMetadata(Subject_ID[0])
+
+function optionChanged(newSubject_ID){
+    buildCharts(newSubject_ID)
+    buildMetadata(newSubject_ID)
+    }
+}
+)}
+
+init()
+
